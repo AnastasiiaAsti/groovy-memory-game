e713b1cc9b7a99ccb6d0c66a51bc86b5cab6dabc
Asti <astistateofmind@gmail.com>
2022-06-04 11:19:26 -0700

    add cards array

---
+++ js/main.js
@@ -0,0 +1,76 @@
+//variables 
+
+const cards = [
+    {
+        name: 'card1',
+        img: 'css/cards/1.jpeg'
+    },
+    {
+        name: 'card2',
+        img: 'css/cards/2.jpeg'
+    },
+    {
+        name: 'card3',
+        img: 'css/cards/3.jpeg'
+    },
+    {
+        name: 'card4',
+        img: 'css/cards/4.jpeg'
+    },
+    {
+        name: 'card5',
+        img: 'css/cards/5.jpeg'
+    },
+    {
+        name: 'card6',
+        img: 'css/cards/6.jpeg'
+    },
+    {
+        name: 'card7',
+        img: 'css/cards/7.jpeg'
+    },
+    {
+        name: 'card8',
+        img: 'css/cards/8.jpeg'
+    },
+    {
+        name: 'card9',
+        img: 'css/cards/9.jpeg'
+    },
+    {
+        name: 'card10',
+        img: 'css/cards/10.jpeg'
+    },
+    {
+        name: 'card11',
+        img: 'css/cards/11.jpeg'
+    },
+    {
+        name: 'card12',
+        img: 'css/cards/12.jpeg'
+    },
+    {
+        name: 'card13',
+        img: 'css/cards/13.jpeg'
+    },
+    {
+        name: 'card14',
+        img: 'css/cards/14.jpeg'
+    },
+    {
+        name: 'card15',
+        img: 'css/cards/15.jpeg'
+    },
+    {
+        name: 'card16',
+        img: 'css/cards/16.jpeg'
+    },
+    {
+        name: 'card17',
+        img: 'css/cards/17.jpeg'
+    },
+    {
+        name: 'card18',
+        img: 'css/cards/18.jpeg'
+    },
+]
