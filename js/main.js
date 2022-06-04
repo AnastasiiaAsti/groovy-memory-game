df50ebd9d55212ac1b750ba8baf08ba617e2cab9
Asti <astistateofmind@gmail.com>
2022-06-04 15:16:25 -0700

    undate array, add second set for each card

--- js/main.js
+++ js/main.js
@@ -1,78 +1,149 @@
 //variables 
 
-const CARDS = [
+const CARDS_ARRAY = [
     {
         name: 'card1',
-        img: 'css/cards/1.jpeg'
+        img: 'cards/1.jpeg'
+    },
+    {
+        name: 'card1',
+        img: 'cards/1.jpeg'
     },
     {
         name: 'card2',
-        img: 'css/cards/2.jpeg'
+        img: 'cards/2.jpeg'
+    },
+    {
+        name: 'card2',
+        img: 'cards/2.jpeg'
     },
     {
         name: 'card3',
-        img: 'css/cards/3.jpeg'
+        img: 'cards/3.jpeg'
+    },
+    {
+        name: 'card3',
+        img: 'cards/3.jpeg'
     },
     {
         name: 'card4',
-        img: 'css/cards/4.jpeg'
+        img: 'cards/4.jpeg'
+    },
+    {
+        name: 'card4',
+        img: 'cards/4.jpeg'
     },
     {
         name: 'card5',
-        img: 'css/cards/5.jpeg'
+        img: 'cards/5.jpeg'
+    },
+    {
+        name: 'card5',
+        img: 'cards/5.jpeg'
     },
     {
         name: 'card6',
-        img: 'css/cards/6.jpeg'
+        img: 'cards/6.jpeg'
+    },
+    {
+        name: 'card6',
+        img: 'cards/6.jpeg'
     },
     {
         name: 'card7',
-        img: 'css/cards/7.jpeg'
+        img: 'cards/7.jpeg'
+    }, {
+        name: 'card7',
+        img: 'cards/7.jpeg'
     },
     {
         name: 'card8',
-        img: 'css/cards/8.jpeg'
+        img: 'cards/8.jpeg'
+    },
+    {
+        name: 'card8',
+        img: 'cards/8.jpeg'
     },
     {
         name: 'card9',
-        img: 'css/cards/9.jpeg'
+        img: 'cards/9.jpeg'
+    },
+    {
+        name: 'card9',
+        img: 'cards/9.jpeg'
     },
     {
         name: 'card10',
-        img: 'css/cards/10.jpeg'
+        img: 'cards/10.jpeg'
+    },
+    {
+        name: 'card10',
+        img: 'cards/10.jpeg'
     },
     {
         name: 'card11',
-        img: 'css/cards/11.jpeg'
+        img: 'cards/11.jpeg'
+    },
+    {
+        name: 'card11',
+        img: 'cards/11.jpeg'
     },
     {
         name: 'card12',
-        img: 'css/cards/12.jpeg'
+        img: 'cards/12.jpeg'
+    },
+    {
+        name: 'card12',
+        img: 'cards/12.jpeg'
     },
     {
         name: 'card13',
-        img: 'css/cards/13.jpeg'
+        img: 'cards/13.jpeg'
+    },
+    {
+        name: 'card13',
+        img: 'cards/13.jpeg'
     },
     {
         name: 'card14',
-        img: 'css/cards/14.jpeg'
+        img: 'cards/14.jpeg'
+    },
+    {
+        name: 'card14',
+        img: 'cards/14.jpeg'
     },
     {
         name: 'card15',
-        img: 'css/cards/15.jpeg'
+        img: 'cards/15.jpeg'
+    },
+    {
+        name: 'card15',
+        img: 'cards/15.jpeg'
     },
     {
         name: 'card16',
-        img: 'css/cards/16.jpeg'
+        img: 'cards/16.jpeg'
+    },
+    {
+        name: 'card16',
+        img: 'cards/16.jpeg'
     },
     {
         name: 'card17',
-        img: 'css/cards/17.jpeg'
+        img: 'cards/17.jpeg'
+    },
+    {
+        name: 'card17',
+        img: 'cards/17.jpeg'
     },
     {
         name: 'card18',
-        img: 'css/cards/18.jpeg'
+        img: 'cards/18.jpeg'
     },
+    {
+        name: 'card18',
+        img: 'cards/18.jpeg'
+    }
 ]
 
 const BOARD = document.getElementById('board')
